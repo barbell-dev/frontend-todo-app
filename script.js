ed766107b19444ef4d2691f246fccb2e66aa1d04
Varun Dunna <varun.dunna2524@gmail.com>
2024-08-25 17:38:45 +0530

    state added

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+let todoList = [];
+window.onload = function () {
+  temp = localStorage.getItem("todoList");
+  if (temp) {
+    todoList = temp.split(",");
+    // todoList.push(temp);
+    // console.log(temp);
+  }
+  // console.log(todoList);
+  displayTodo();
+  // console.log(typeof temp);
+};
+function displayTodo() {
+  let todos = "";
+  for (let i = 0; i < todoList.length; i++) {
+    todos += `<div class="new-element" id='${todoList[i]}'>
+    <h4>${todoList[i]}</h4><button class="edit">Edit</button><button class="delete" id="delete${todoList[i]}">Delete</button></div>`;
+  }
+  console.log(todos);
+  document.querySelector("#todoList").innerHTML = todos;
+  activateEditListener();
+  activateDeleteListener();
+}
+function activateEditListener() {
+  let editButton = document.querySelectorAll(".edit");
+  editButton.forEach((eb, i) => {
+    eb.addEventListener("click", () => {
+      editTodo(i);
+    });
+  });
+}
+function editTodo(j) {
+  let editButtons = document.querySelectorAll(".edit");
+  for (let i = 0; i < editButtons.length; i++) {
+    if (editButtons[i].parentElement.children[0].innerText == todoList[j]) {
+      let newName = prompt(
+        "Enter new name for todo",
+        editButtons[i].parentElement.children[0].innerHTML
+      );
+      // newName.init;
+      if (newName) {
+        // let temp =
+        todoList[j] = newName;
+        localStorage.setItem("todoList", todoList);
+        // console.log(todoList[j]);
+        location.reload();
+      } else {
+        // alert("Edit has been cancelled.");
+        return;
+      }
+    }
+  }
+  // console.log(todoList);
+  location.reload;
+}
 function addToDo() {
   let sample = document.querySelector(".input-add-todo").value;
   let newNode = document.createElement("div");
@@ -28,26 +83,45 @@ function addToDo() {
   newNode.appendChild(deleteButton);
   newNode.className = "new-element";
   newNode.id = addToDoText;
+  // console.log(newNode.innerHTML);
   //   newNode.innerHTML += editButton;
-  let parentNode = document.querySelector(".input-add-todo").parentElement;
+  let parentNode = document.querySelector("#todoList");
   //   let editeditButton = document.g
   parentNode.appendChild(newNode);
+  // console.log(parentNode.children.length);
+  // console.log(typeof todoList);
+  todoList.push(h4.innerText);
+  localStorage.setItem("todoList", todoList);
   //   console.log(typeof document.getElementById("ad"))
   deleteButton.onclick = function () {
     newNode.parentElement.removeChild(document.getElementById(addToDoText));
   };
   document.querySelector(".input-add-todo").value = "";
+  console.log(editButton.parentElement.innerHTML);
   editButton.onclick = function () {
-    let newName = prompt("Enter new name for todo");
+    let newName = prompt(
+      "Enter new name for todo",
+      editButton.parentElement.children[0].innerHTML
+    );
+    // newName.init;
     if (newName) {
       let node = document.querySelector("#" + addToDoText).children[0];
-      console.log(node);
+      // console.log(node);
       node.innerHTML = newName;
+      // let temp =
+      for (let i = 0; i < todoList.length; i++) {
+        if (todoList[i] == editButton.parentElement.children[0]) {
+          todoList[i] = newName;
+        }
+      }
     } else {
       // alert("Edit has been cancelled.");
       return;
     }
   };
+
+  // localStorage.setItem("tasks", document.querySelectorAll(".new-element"));
+  // console.log(localStorage);
 }
 // p.appendChild(new);
 function search() {
